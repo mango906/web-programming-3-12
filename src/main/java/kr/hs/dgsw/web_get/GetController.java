acadaa8574c80d14cdf2aee39387a045279f9fc2
mango906 <minkb01@dgsw.hs.kr>
2019-03-12 16:32:33 +0900

    Initialize

---
+++ src/main/java/kr/hs/dgsw/web_get/GetController.java
@@ -0,0 +1,24 @@
+package kr.hs.dgsw.web_get;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class GetController {
+
+    @GetMapping("/greeting")
+    public String sayHello(@RequestParam(required = false, defaultValue = "Hello") String say,
+                           @RequestParam(required = false, defaultValue = "NONAME") String to) {
+        return say + " " + to;
+    }
+
+    @GetMapping(value = {"/greeting1/{say}/{to}", "/greeting1/{say}", "/greeting1//{to}", "/greeting1/"})
+    public String sayHello1(@PathVariable Optional<String> say, @PathVariable Optional<String> to) {
+        String tmp = to.isPresent() ? to.get() : "NONAME";
+        return (say.isPresent() ? say.get() : "hello") + " " + tmp;
+    }
+}
